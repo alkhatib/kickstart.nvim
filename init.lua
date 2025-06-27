1902e3f433d18eb0c5a65559cb080384e9c49701
Anas Alkhatib <anas.alkhatib@gmail.com>
2025-06-27 11:37:54 -0400

    'mini'

--- init.lua
+++ init.lua
@@ -849,6 +849,7 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+      require('mini.git').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
